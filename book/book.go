436e7eff281ea6a21a9beb6fe6d792f636f31c85
Harssh <Harsshshrivastava@sullivancotter.com>
2020-05-17 12:02:22 -0500

    Add ORM and gitignore

--- book/book.go
+++ book/book.go
@@ -1,25 +1,64 @@
 package book
 
-import(
+import (
 	"github.com/gofiber/fiber"
+	"github.com/harssh/books_gofiber/database"
+	"github.com/jinzhu/gorm"
 )
 
-func GetBooks(c *fiber.Ctx)  {
-	c.Send("All Books")
+type Book struct {
+	gorm.Model
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Rating int    `json:"rating"`
 }
 
-func GetBook(c *fiber.Ctx)  {
-	c.Send("A Single Books")
+func GetBooks(c *fiber.Ctx) {
+	db := database.DBConn
+	var books []Book
+	db.Find(&books)
+	c.JSON(books)
 }
 
-func UpdateBook(c *fiber.Ctx)  {
+func GetBook(c *fiber.Ctx) {
+	id := c.Params("id")
+  db := database.DBConn
+	var book Book
+	db.Find(&book, id)
+	c.JSON(book)
+}
+
+func UpdateBook(c *fiber.Ctx) {
 	c.Send("A update Single Books")
 }
 
-func NewBook(c *fiber.Ctx)  {
-	c.Send("Add new Book")
+func NewBook(c *fiber.Ctx) {
+	db := database.DBConn
+	var book Book
+	book.Title = "1984"
+	book.Author = "George Orwell"
+	book.Rating = 5
+
+	db.Create(&book)
+	if db.NewRecord(book) != false {
+		c.Send("Record created.")
+		} else {
+		c.Send("Creation Failed")
+	}
+ c.JSON(book)
 }
 
-func DeleteBook(c *fiber.Ctx)  {
-	c.Send("Delete a book")
+func DeleteBook(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+
+	var book Book
+	db.Find(&book, id)
+	if book.Title == "" {
+		c.Status(500).Send("No book found with given ID")
+		return
+	}
+
+	db.Delete(&book)
+	c.Send("Book Successfully created.")
 }
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber"
 	"github.com/harssh/books_gofiber/book"
 	"github.com/harssh/books_gofiber/database"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func helloWorld(c *fiber.Ctx) {
 	c.Send("Hello, World!")
 }
-
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/api/v1/book/:id", book.GetBook)
@@ -20,12 +23,22 @@ func setupRoutes(app *fiber.App) {
 
 }
 
-func initDatabase()  {
-	var
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("DB connected.")
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("DB migrated")
 }
 
 func main() {
 	app := fiber.New()
+	initDatabase()
+
+	defer database.DBConn.Close()
 	setupRoutes(app)
 	app.Listen(3000)
 
